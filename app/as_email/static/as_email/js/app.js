936ab67e4e87c6bbd5d3eba9480864c681643811
Scanner <scanner@apricot.com>
2023-10-18 17:07:48 -0700

    GH-46: Switching over to ESM for view, ie: modules. Unforunately `map` does not seem to be working properly.
    Going to have to modify all the JS files to import from the right URL when we switch from `development` to `production` versions of vue.

--- app/as_email/static/as_email/js/app.js
+++ app/as_email/static/as_email/js/app.js
@@ -1,25 +1,32 @@
 // AS Email App Vue/JS file
-const initialData = JSON.parse(document.querySelector('#vue-data').textContent);
-fetch(initialData.email_accounts_data[0].url,{method:"OPTIONS"}).
-    then((response) => {
-        if (!response.ok) {
-            alert("HTTP Error: " + response.status);
-        }
-        return response.body;
-    })
-    .then((response) => {
-        alert(response);
-    });
+//
+import { createApp, ref, onMounted } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
-function setupASEmail() {
-    let app = new Vue({
-        el: "#vue-app",
-        delimiters: ['[[', ']]'],
-        data: function() {
-            return JSON.parse(document.querySelector('#vue-data').textContent);
-        },
-    });
-}
-
-setupASEmail();
+// Get the data that the as_email service filled the document with that
+// contains information about all the EmailAccount's.
+//
+// It also contains the DRF `options` data for the EmailAccount object so we
+// know what fields exist, what types they are, read only or not, and
+// descriptions.
+//
+const initialData = JSON.parse(document.getElementById('vue-data').textContent);
+console.log(initialData.email_account_list_url);
+// Hook up the 'bulma-collapsible' support js code for collapsing and expanding
+// the EmailAccount cards.
+//
 const emailAccountCards = bulmaCollapsible.attach(".is-collapsible");
+
+let res = await fetch(initialData.email_accounts_data[0].url, {method:"OPTIONS"});
+let emailAccountFieldsOptions = await res.json();
+let emailAccountFields = emailAccountFieldsOptions.actions.PUT;
+console.log(emailAccountFields);
+
+createApp({
+    setup() {
+        const myTitle = ref('Hello Vue!');
+        return {
+            myTitle
+        };
+    },
+    delimiters: ['[[', ']]'],
+}).mount('#vue-app');
