3ecaecbb70c2ba72ef81dbefa33aa2a729c2545a
Scanner <scanner@apricot.com>
2023-10-22 18:09:23 -0700

    GH-47, GH-50: Add `aliases` to the EmailAccountForm. Set the list of valid choices for `alias_for` and `aliases`

--- app/as_email/forms.py
+++ app/as_email/forms.py
@@ -20,6 +20,9 @@ from .models import EmailAccount
 ########################################################################
 #
 class EmailAccountForm(forms.ModelForm):
+    aliases = forms.ModelMultipleChoiceField(queryset=None)
+    alias_for = forms.ModelMultipleChoiceField(queryset=None)
+
     class Meta:
         model = EmailAccount
         fields = [
@@ -28,7 +31,6 @@ class EmailAccountForm(forms.ModelForm):
             "spam_delivery_folder",
             "spam_score_threshold",
             "alias_for",
-            # "aliases",
             "forward_to",
         ]
 
@@ -39,10 +41,28 @@ class EmailAccountForm(forms.ModelForm):
         Make the EmailAccountForm crispy.
         """
         super().__init__(*args, **kwargs)
-        instance = kwargs["instance"]
         self.helper = FormHelper()
-        self.helper.form_id = f"email-account-form-{instance.pk}"
-        self.helper.form_class = "blueForms"
+        if self.instance:
+            # The `alias_for` and `aliases` field can only list EmailAccount's
+            # that have the same owner as the EmailAccount self.instance refers
+            # to. Also you can not alias to yourself, so exclude this instance
+            # from the queryset of possible valid instances.
+            #
+            self.fields["aliases"].queryset = EmailAccount.objects.filter(
+                owner=self.instance.owner
+            ).exclude(pk=self.instance.pk)
+            self.fields["alias_for"].queryset = EmailAccount.objects.filter(
+                owner=self.instance.owner
+            ).exclude(pk=self.instance.pk)
+
+            # and give each form entry a unique html/css id with a known
+            # pattern that our vue code can use to find this form on the page.
+            #
+            self.helper.form_id = f"email-account-form-{self.instance.pk}"
+        else:
+            self.helper.form_id = "new-email-account-form-id"
+
+        # self.helper.form_class = "blueForms"
         self.helper.form_method = "post"
         self.helper.form_action = "as_email:email-account-detail"
 
