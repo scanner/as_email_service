1bd2cf36cd5018df8b827167ca67e8a4d8790cf9
Scanner <scanner@apricot.com>
2023-10-26 12:32:20 -0700

    more small bits for our EmailAccount and MessageFilterRule vue components

--- app/as_email/static/as_email/js/email_account.js
+++ app/as_email/static/as_email/js/email_account.js
@@ -1,25 +1,37 @@
 // Vue Component for an EmailAccount
 //
 import { ref } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import MessageFilterRule from './message_filter_rule.js';
 
 export default {
     props: [
-        'alias_for',
-        'aliases',
-        'autofile_spam',
-        'deactivated',
-        'deactivated_reason',
-        'delivery_method',
-        'email_address',
-        'num_bounces',
-        'owner',
-        'pk',
-        'server',
-        'spam_delivery_folder',
-        'spam_score_threshold',
-        'url'
+        "alias_for",
+        "aliases",
+        "autofile_spam",
+        "deactivated",
+        "deactivated_reason",
+        "delivery_method",
+        "email_address",
+        "num_bounces",
+        "owner",
+        "pk",
+        "server",
+        "spam_delivery_folder",
+        "spam_score_threshold",
+        "url",
     ],
-    setup(props) {
-        // access props.emailAccountData
-    }
+    // An event sent to the parent component to indicate that aliases or
+    // alias_for has changed in this component and other EmailAccount
+    // components should have their aliases,alias_for's re-pulled from the
+    // server because they may have changed.
+    //
+    emits: [ 'update-aliases' ],
+    components: {
+        MessageFilterRule: MessageFilterRule
+    },
+    setup(props, ctx) {
+        // access props.alias_for, etc.
+    },
+    template: `
+`
 }
---
+++ app/as_email/static/as_email/js/message_filter_rule.js
@@ -0,0 +1,17 @@
+// Vue Component for a MessageFilterRule
+//
+import { ref } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+export default {
+    props: [
+        "header",
+        "pattern",
+        "action",
+        "destination",
+        "order",
+    ],
+    setup(props) {
+        // access props.header, etc.
+    },
+    template: ``
+}
