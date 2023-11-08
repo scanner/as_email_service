1fe3f87658be4edda2c5341b646a558fdbc80b46
Scanner <scanner@apricot.com>
2023-11-07 16:52:06 -0800

    GH-64 - collectstatic is run during image building.
    Fails to run at run time because the static files are owned by root, not app.

--- app/scripts/start_app.sh
+++ app/scripts/start_app.sh
@@ -5,8 +5,9 @@ set -o pipefail
 set -o nounset
 
 wait-for-it --service redis:6379 -- echo "Redis available"
-# XXX We collectstatic in the docker image build step so we should probably
-#     remove this.
-/venv/bin/python /app/manage.py collectstatic --clear --no-input --verbosity 0
+
+echo "Running django migrations.."
 /venv/bin/python /app/manage.py migrate
+
+echo "Starting uvicorn.."
 /venv/bin/gunicorn config.asgi --bind 0.0.0.0:8000 --chdir=/app -w 4 -k uvicorn.workers.UvicornWorker
