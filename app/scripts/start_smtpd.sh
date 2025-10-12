b9a37290bba5fb0fc4661343857c3f3bc216e005
Scanner <scanner@apricot.com>
2025-10-11 17:56:49 -0700

    Fix start_smtpd.sh
    Had left over code from when we were going to have to smtpd demons - one for as_email users to use and one for everyone else.

--- app/scripts/start_smtpd.sh
+++ app/scripts/start_smtpd.sh
@@ -8,14 +8,14 @@ wait-for-it --service redis:6379 -- echo "Redis available"
 
 # Set LISTEN_HOST to `0.0.0.0` if not already set.
 #
-: "${OUTBOUND_SMTPD_LISTEN_HOST:=0.0.0.0}"
-: "${OUTBOUND_SMTPD_PORT:=19246}"
-: "${OUTBOUND_SMTPD_CERT:=/mnt/ssl/ssl_crt.pem}"
-: "${OUTBOUND_SMTPD_KEY:=/mnt/ssl/ssl_key.pem}"
+: "${SMTPD_LISTEN_HOST:=0.0.0.0}"
+: "${SMTPD_PORT:=587}"
+: "${SMTPD_CERT:=/mnt/ssl/ssl_crt.pem}"
+: "${SMTPD_KEY:=/mnt/ssl/ssl_key.pem}"
 
 echo "Starting SMTP daemon"
 /venv/bin/python /app/manage.py aiosmtpd \
-                 --listen_port="${OUTBOUND_SMTPD_PORT}" \
-                 --listen_host="${OUTBOUND_SMTPD_LISTEN_HOST}" \
-                 --ssl_key=/mnt/ssl/"${OUTBOUND_SMTPD_KEY}" \
-                 --ssl_cert="${OUTBOUND_SMTPD_CERT}"
+                 --listen_port="${SMTPD_PORT}" \
+                 --listen_host="${SMTPD_LISTEN_HOST}" \
+                 --ssl_key="${SMTPD_KEY}" \
+                 --ssl_cert="${SMTPD_CERT}"
